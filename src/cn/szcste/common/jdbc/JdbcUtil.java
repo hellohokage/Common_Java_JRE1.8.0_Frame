572f911f330c381e78e66103c257fc5c160e5d11
hellohokage <szcste@163.com>
2018-05-20 16:25:52 +0800

    '增加了c3p0连接池技术框架，旧版本无连接池管理。具体删除了cn.szcste.common.jdbc.utils.ParseProperties,优化了cn.szcste.common.jdbc.JdbcUtil.getConnection()

--- src/cn/szcste/common/jdbc/JdbcUtil.java
+++ src/cn/szcste/common/jdbc/JdbcUtil.java
@@ -1,24 +1,18 @@
 package cn.szcste.common.jdbc;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import cn.szcste.common.jdbc.utils.ParseProperties;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class JdbcUtil {
 	public static Connection getConnection() throws SQLException, ClassNotFoundException {
 		// TODO Auto-generated method stub
+		
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource("second_config");
 
-		ParseProperties parseProperties = new ParseProperties();
-
-		Class.forName(parseProperties.driver);
-
-		Connection connection = DriverManager.getConnection(parseProperties.url, parseProperties.user,
-				parseProperties.password);
-
-		return connection;
+		return comboPooledDataSource.getConnection();
 	}
 
 	public static void closeAll(Connection connection, PreparedStatement prepareStatement) {
